1fcf9ed22a0b9048047cb6a1b1143a26d118329b
yjkim <kimyeji203@naver.com>
2023-05-27 01:29:39 +0900

    빌드 오류 수정

--- src/main/java/com/dailystudy/swinglab/service/business/auth/controller/AuthController.java
+++ src/main/java/com/dailystudy/swinglab/service/business/auth/controller/AuthController.java
@@ -1,22 +1,15 @@
 package com.dailystudy.swinglab.service.business.auth.controller;
 
 import com.dailystudy.swinglab.service.business.auth.service.AuthService;
-import com.dailystudy.swinglab.service.business.domain.JwtToken;
 import com.dailystudy.swinglab.service.business.domain.entity.user.User;
-import com.dailystudy.swinglab.service.framework.SwinglabConst;
-import com.dailystudy.swinglab.service.framework.auth.TokenProvider;
 import com.dailystudy.swinglab.service.framework.http.response.PlatformResponseBuilder;
 import com.dailystudy.swinglab.service.framework.http.response.domain.SuccessResponse;
 import com.dailystudy.swinglab.service.framework.http.uris.AuthUriConts;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
