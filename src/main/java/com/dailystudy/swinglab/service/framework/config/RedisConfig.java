e8f72064ab26385cc78c211bdbc3a2bb5ff73c51
kimyeji203 <kimyeji203@naver.com>
2023-06-11 00:21:36 +0900

    redis pwd 설정

--- src/main/java/com/dailystudy/swinglab/service/framework/config/RedisConfig.java
+++ src/main/java/com/dailystudy/swinglab/service/framework/config/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -24,7 +25,11 @@ public class RedisConfig
     @Bean
     public RedisConnectionFactory redisConnectionFactory ()
     {
-        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisProperties.getHost());
+        redisStandaloneConfiguration.setPort(redisProperties.getPort());
+        redisStandaloneConfiguration.setPassword(redisProperties.getPassword());
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
